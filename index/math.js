3462000f3642d018b48cc3acba19cb5f51abd589
RIMOPA <santino.rimondotto@gmail.com>
2024-04-13 13:44:44 -0300

    style math and add to header

--- index/math.js
+++ index/math.js
@@ -1,14 +1,14 @@
-//Get annual rate of return on any investment
-/*
-ci=float(input("Ingrese el capital inicial:"))
-cf=float(input("Ingrese el capital final:"))
-ti=float(input("Ingrese en días el tiempo que se mantuvo el crédito:"))
-print("el rendimiento anual sería de ", ((((cf-ci)/ci)/ti)*365.4)*100, "%") */
 function annualperf() {
   ci = document.getElementById("initialCapital").value;
   cf = document.getElementById("finalCapital").value;
   ti = document.getElementById("daysInvested").value;
-
-  document.getElementById("resultannualperf").innerText =
-    ((cf - ci) / ci / ti) * 365 * 100 + "%";
+  if ((ci && cf && ti) != "") {
+    document.getElementById("resultannualperf").innerText =
+      ((cf - ci) / ci / ti) * 365 * 100 + "%";
+    document.getElementById("resultannualperf").style.color = document.getElementById("body").style.color
+  } else {
+    document.getElementById("resultannualperf").innerText =
+      "Some arguments are missing";
+    document.getElementById("resultannualperf").style.color = "#C70039";
+  }
 }
