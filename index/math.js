7402c933457dc9dac09e549dd28560212feee82d
RIMOPA <santino.rimondotto@gmail.com>
2024-04-14 11:32:04 -0300

    new cool distance math

--- index/math.js
+++ index/math.js
@@ -1,14 +1,41 @@
+//initial execution
+distance2points();
+annualperf();
+//Get annual performance of any investment
 function annualperf() {
-  ci = document.getElementById("initialCapital").value;
-  cf = document.getElementById("finalCapital").value;
-  ti = document.getElementById("daysInvested").value;
+  ci = document.getElementById("anperf.initialCapital").value;
+  cf = document.getElementById("anperf.finalCapital").value;
+  ti = document.getElementById("anperf.daysInvested").value;
   if ((ci && cf && ti) != "") {
-    document.getElementById("resultannualperf").innerText =
+    document.getElementById("anperf.result").innerText =
       ((cf - ci) / ci / ti) * 365 * 100 + "%";
-    document.getElementById("resultannualperf").style.color = document.getElementById("body").style.color
+    document.getElementById("anperf.result").style.color =
+      document.getElementById("body").style.color;
   } else {
-    document.getElementById("resultannualperf").innerText =
+    document.getElementById("anperf.result").innerText =
       "Some arguments are missing";
-    document.getElementById("resultannualperf").style.color = "#C70039";
+    document.getElementById("anperf.result").style.color = "#C70039";
+  }
+}
+//Get different kinds of distance between two points on a plane
+function distance2points() {
+  p = document.getElementById("d2p.p").value;
+  x1 = document.getElementById("d2p.x1").value;
+  y1 = document.getElementById("d2p.y1").value;
+  x2 = document.getElementById("d2p.x2").value;
+  y2 = document.getElementById("d2p.y2").value;
+  if ((p && x1 && y1 && x2 && y2) != "") {
+    document.getElementById("d2p.result").innerText = Math.pow(
+      Math.pow(Math.abs(x2 - x1), p) + Math.pow(Math.abs(y2 - y1), p),
+      1 / p
+    );
+    document.getElementById("d2p.result").style.color =
+      document.getElementById("body").style.color;
+    document.getElementById("d2p.result").title =
+      document.getElementById("d2p.result").innerText + " mandarinas";
+  } else {
+    document.getElementById("d2p.result").innerText =
+      "Some arguments are missing";
+    document.getElementById("d2p.result").style.color = "#C70039";
   }
 }
