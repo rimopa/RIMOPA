22a22a666fdc3a65a64e3b374aa5671268ab5fce
RIMOPA <santino.rimondotto@gmail.com>
2024-08-11 22:54:44 -0300

    simplifying it a bit. Could have done it from the beggining

--- general/capitalization-title.js
+++ general/capitalization-title.js
@@ -1,9 +1,3 @@
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max);
-}
-function dec2bin(dec) {
-  return (dec >>> 0).toString(2);
-}
 function setCharAt(str, index, chr) {
   if (index > str.length - 1) return str;
   return str.substring(0, index) + chr + str.substring(index + 1);
@@ -12,12 +6,8 @@ function setCharAt(str, index, chr) {
 function capitalizationTitle() {
   var title = document.getElementById("pageTitle").innerText;
   setTimeout(() => {
-    var capitalizationCode = dec2bin(getRandomInt(2 ** title.length));
-    capitalizationCode = ("0".repeat(title.length) + capitalizationCode).slice(
-      -title.length
-    ); //adding 0 pad so It doesn't make letter N°1 always uppercase (except when number is 0)
     for (let index = 0; index < title.length; index++) {
-      if (capitalizationCode[index] == "1") {
+      if (Math.round(Math.random()) == "1") {
         title = setCharAt(title, index, title[index].toUpperCase());
       } else {
         title = setCharAt(title, index, title[index].toLowerCase());
