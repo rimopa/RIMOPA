d97cf77c670ec67408e6f0dea9e0295039b12acf
RIMOPA <santino.rimondotto@gmail.com>
2024-08-11 22:59:45 -0300

    a little better

--- general/capitalization-title.js
+++ general/capitalization-title.js
@@ -2,19 +2,21 @@ function setCharAt(str, index, chr) {
   if (index > str.length - 1) return str;
   return str.substring(0, index) + chr + str.substring(index + 1);
 }
-
-function capitalizationTitle() {
-  var title = document.getElementById("pageTitle").innerText;
+function capitalizationTimeout() {
   setTimeout(() => {
-    for (let index = 0; index < title.length; index++) {
-      if (Math.round(Math.random()) == "1") {
-        title = setCharAt(title, index, title[index].toUpperCase());
-      } else {
-        title = setCharAt(title, index, title[index].toLowerCase());
-      }
-    }
-    document.getElementById("pageTitle").innerText = title;
-    capitalizationTitle();
-  }, "200");
+    updateTitleCapitalization();
+  }, "500");
 }
-capitalizationTitle();
+function updateTitleCapitalization() {
+  var title = document.getElementById("pageTitle").innerText;
+  for (let index = 0; index < title.length; index++) {
+    if (Math.round(Math.random()) == "1") {
+      title = setCharAt(title, index, title[index].toUpperCase());
+    } else {
+      title = setCharAt(title, index, title[index].toLowerCase());
+    }
+  }
+  document.getElementById("pageTitle").innerText = title;
+  capitalizationTimeout();
+}
+updateTitleCapitalization();
